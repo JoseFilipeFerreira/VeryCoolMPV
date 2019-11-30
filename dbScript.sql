830df22ed2c053d75e418a67f1a6735d21d7fedd
hitler <a85504@alunos.uminho.pt>
2019-11-30 12:27:13 +0000

    Db script

---
+++ dbScript.sql
@@ -0,0 +1,63 @@
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+CREATE SCHEMA IF NOT EXISTS `DSS` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ;
+USE `DSS` ;
+
+-- -----------------------------------------------------
+-- Table `DSS`.`Utilizadores`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `DSS`.`Utilizadores` (
+  `email` VARCHAR(45) NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  `passwd` VARCHAR(45) NULL DEFAULT NULL,
+  PRIMARY KEY (`email`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_unicode_ci;
+
+
+-- -----------------------------------------------------
+-- Table `DSS`.`Media`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `DSS`.`Media` (
+  `name` VARCHAR(45) NOT NULL,
+  `path` VARCHAR(45) NULL DEFAULT NULL,
+  `owner` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`name`),
+  INDEX `fk_Media_Utilizadores_idx` (`owner` ASC) VISIBLE,
+  CONSTRAINT `fk_Media_Utilizadores`
+    FOREIGN KEY (`owner`)
+    REFERENCES `DSS`.`Utilizadores` (`email`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_unicode_ci;
+
+
+-- -----------------------------------------------------
+-- Table `DSS`.`Playlist`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `DSS`.`Playlist` (
+  `Utilizadores_email` VARCHAR(45) NOT NULL,
+  `Media_name` VARCHAR(45) NOT NULL,
+  `playlist_id` INT(11) NOT NULL,
+  `is_shared` TINYINT NOT NULL,
+  PRIMARY KEY (`Utilizadores_email`, `Media_name`, `playlist_id`),
+  INDEX `fk_Utilizadores_has_Media_Media1_idx` (`Media_name` ASC) VISIBLE,
+  INDEX `fk_Utilizadores_has_Media_Utilizadores1_idx` (`Utilizadores_email` ASC) VISIBLE,
+  CONSTRAINT `fk_Utilizadores_has_Media_Media1`
+    FOREIGN KEY (`Media_name`)
+    REFERENCES `DSS`.`Media` (`name`),
+  CONSTRAINT `fk_Utilizadores_has_Media_Utilizadores1`
+    FOREIGN KEY (`Utilizadores_email`)
+    REFERENCES `DSS`.`Utilizadores` (`email`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_unicode_ci;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
