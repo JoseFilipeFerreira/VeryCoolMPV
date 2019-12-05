de2f3f243de625ba92d5dae979ef0998748ff0b9
hitler <a85504@alunos.uminho.pt>
2019-12-05 18:06:58 +0000

    Frick

--- dbScript.sql
+++ dbScript.sql
@@ -1,8 +1,19 @@
+-- MySQL Workbench Forward Engineering
 
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema DSS
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema DSS
+-- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `DSS` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ;
 USE `DSS` ;
 
@@ -13,6 +24,7 @@ CREATE TABLE IF NOT EXISTS `DSS`.`Utilizadores` (
   `email` VARCHAR(45) NOT NULL,
   `name` VARCHAR(45) NOT NULL,
   `passwd` VARCHAR(45) NULL DEFAULT NULL,
+  `admin` TINYINT NOT NULL,
   PRIMARY KEY (`email`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
